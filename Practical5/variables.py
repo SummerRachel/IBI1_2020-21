ea4143759a6cdf4f87e5ecbfc982ef0e2ebd25f1
xia ziyu <ziyux.20@intl.zju.edu.cn>
2021-03-10 11:44:04 +0800

    add

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -5,6 +5,21 @@ d=abs(a-c)
 e=abs(a-b)
 print"a=",a,"b=",b,"c=",c,"d=",d,"e=",e
 if d-e<0:
- print("e is bigger")
+ print("d<e")
+elif d-e>0:
+ print("d>e")
 else:
- print("d is bigger")
+ print("d=e")
+
+X=True
+Y=False
+Z=(X and not Y)or(Y and not X)
+if(Z==True):
+ print"Z is true"
+else:
+ print"Z is false"
+W=X!=Y
+if(W==Z):
+ print"W and Z are always the same"
+else:
+ print"W and Z are not always the same"
