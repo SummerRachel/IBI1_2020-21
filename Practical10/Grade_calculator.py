977ce141771212e8115f205f3dc3a37fdb07277d
SummerRachel <79951446+SummerRachel@users.noreply.github.com>
2021-05-18 20:55:44 +0800

    Update Grade_calculator.py

--- Practical10/Grade_calculator.py
+++ Practical10/Grade_calculator.py
@@ -9,5 +9,7 @@ class Grade(object):
 		self.final=self.code*0.4+self.poster*0.3+self.exam*0.3
 		return self.final
 #use an example to check the code
+print('Example: Name:zy Code_score:100 Poster_score:90 Exam_score:100')
+print('Final score is:')
 example=Grade('zy',100,90,100,0)
 print('Example final score:',example.calculator())
